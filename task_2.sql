8d758745e26e8af61053b6de1d714ddbd05df0d4
yousseflj9 <youssefmomen@outlook.com>
2025-10-17 20:06:53 +0100

    clear

--- task_2.sql
+++ task_2.sql
@@ -1,4 +1,6 @@
-REATE TABLE IF NOT EXISTS Authors (
+use alx_book_store;
+
+CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
