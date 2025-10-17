ca2bd3266a7daebf2b7f3b03a6a112d8f096ea25
yousseflj9 <youssefmomen@outlook.com>
2025-10-17 19:35:21 +0100

    task_1

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,18 +1,20 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS Books (
-
-    book_id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(130),
-    author_id INT FOREIGN KEY REFERENCES Authors(author_id),
-    price DOUBLE,
-    publication_date DATE
-);
 CREATE TABLE IF NOT EXISTS Authors (
 
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215)
+);
+
+CREATE TABLE IF NOT EXISTS Books (
+
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(130),
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Customers (
@@ -25,14 +27,17 @@ CREATE TABLE IF NOT EXISTS Customers (
 CREATE TABLE IF NOT EXISTS Orders (
 
     order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_id INT FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
 CREATE TABLE IF NOT EXISTS Order_Items (
 
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
-    order_id INT FOREIGN KEY REFERENCES Orders(order_id),
-    book_id INT FOREIGN KEY REFERENCES Books(book_id),
-    quantity DOUBLE
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
